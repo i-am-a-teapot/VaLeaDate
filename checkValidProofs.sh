555ae66915321f6207a090852bae9e5803f15ec5
Jonas Bodingbauer <jonas.bodingbauer@tuwien.ac.at>
2026-06-25 15:39:11 +0200

    Add relevant build files

--- checkValidProofs.sh
+++ checkValidProofs.sh
@@ -3,8 +3,8 @@
 export VAMPLEAN_PATH=/home/jonas/VampireBenchmarking/vamp_lean/VampLean 
 export VAMPIRE_BINARY=/home/jonas/vampire/build/vampire 
 export LEAN_BINARY=/home/jonas/.elan/toolchains/leanprover--lean4---v4.29.0/bin/lean
-
+export TPTP=/home/jonas/TPTP-v9.2.1
 for proof in testVampireProofs/postprocessed_cnf/*.p.log; do
   echo "Running good proof: $proof"
-  time ./valeadate "$proof" --assume-thm --allow-axiom-mismatch --negc-as-thm
+  time ./valeadate "$proof" --assume-thm --allow-axiom-mismatch --negc-as-thm -t 10
 done
---
+++ project/plugins.sbt
@@ -0,0 +1,2 @@
+// Assembly plugin for creating fat JAR for GraalVM native-image
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "2.1.1")
