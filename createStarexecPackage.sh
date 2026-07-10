5fc26cd1e0e58f63dcc67cddf3f072fb4ffd1458
Jonas Bodingbauer <jonas.bodingbauer@tuwien.ac.at>
2026-07-10 17:06:33 +0200

    Add vamplean lib to package

--- createStarexecPackage.sh
+++ createStarexecPackage.sh
@@ -19,13 +19,15 @@ make native-static
 echo "Package stuff:..."
 
 mkdir -p starexecDist/bin
-mkdir starexecDist/requirements
+mkdir -p starexecDist/requirements
 
 cp valeadate-musl starexecDist/bin/valeadate-musl
 cp vampire/build/vampire starexecDist/bin/vampire
 cp starexec/* starexecDist/bin/
 cp -r requirements/elan starexecDist/requirements/elan
+cp -r vamplean starexecDist/vamplean
 
 echo "Creating tarball..."
 tar -czf starexecDist.tar.gz -C starexecDist .
-
+echo "Cleaning up..."
+rm -rf starexecDist
